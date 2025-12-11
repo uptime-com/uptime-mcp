8925092953e73a5a6f37453c7e888f5717afc409
Mikhail Lukianchenko <42915+mikluko@users.noreply.github.com>
2025-12-11 14:54:03 +0100

    Add FluxCD module and namespace resource to uptime-mcp-dev workspace
    - Creates `kubernetes_namespace_v1` resource for `uptime-mcp`.
    - Configures FluxCD module for GitOps integration with `uptime-mcp` repo.

--- deploy/terraform/workspaces/uptime-mcp-dev/main.tf
+++ deploy/terraform/workspaces/uptime-mcp-dev/main.tf
@@ -4,3 +4,21 @@ module "awsecr" {
   name       = each.value.name
   mutability = each.value.mutability
 }
+
+resource "kubernetes_namespace_v1" "this" {
+  metadata {
+    name = "uptime-mcp"
+  }
+  wait_for_default_service_account = true
+}
+
+module "fluxcd" {
+  source            = "git::ssh://git@github.com/uptime-com/uptfmods.git//fluxcd/repo?ref=v0.71"
+  namespace         = one(kubernetes_namespace_v1.this.metadata.*.name)
+  name              = "uptime-mcp"
+  github_repository = "uptime-mcp"
+  ref = {
+    tag = "main"
+  }
+  reconcile_interval = "5m"
+}
